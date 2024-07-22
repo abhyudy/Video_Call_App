62d67a094d38866f20cb1f49c62c82d4dd6342f3
Abhyudya Singh Ujala <abhyudyasinghujala2002@gmail.com>
2024-07-22 22:19:23 +0530

    Add Config.js to .gitignore and remove it from tracking

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
-import "./HomePage.css";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-const HomePage = () => {
+import "./HomePage.css";
+
+function HomePage() {
   const [roomId, setRoomId] = useState("");
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // Update the time every second
+    const intervalId = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
+
+    // Cleanup the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
   const handleRoomIdGenerate = () => {
-    const randomId = Math.random().toString(30).substring(2, 9);
+    const randomId = Math.random().toString(36).substring(2, 9);
     const timestamp = Date.now().toString().substring(-4);
     setRoomId(randomId + timestamp);
   };
@@ -17,6 +30,7 @@ const HomePage = () => {
     }
     navigate(`room/${roomId}?type=one-on-one`);
   };
+
   const handleGroupCall = () => {
     if (!roomId) {
       alert("Please Generate Room Id First");
@@ -24,10 +38,32 @@ const HomePage = () => {
     }
     navigate(`room/${roomId}?type=group-call`);
   };
+
+  const formatDateTime = (date) => {
+    const options = {
+      weekday: "short",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+    return date.toLocaleDateString("en-US", options);
+  };
+
   return (
     <div className="homepage-container">
+      <div className="navbar">
+        <a href="/" className="navbar-name">
+          My MeeT
+        </a>
+        <div className="navbar-date-time">
+          {formatDateTime(currentDateTime)}
+        </div>
+      </div>
       <div className="homepage-content">
-        <h1 className="homepage-title"> Welcome to Video Calling App</h1>
+        <h1 className="homepage-title">Welcome to Video Calling App</h1>
         <p className="homepage-subtitle">
           Start a video call with a randomly generated Room ID
         </p>
@@ -40,17 +76,16 @@ const HomePage = () => {
             readOnly
           />
           <button className="generate-button" onClick={handleRoomIdGenerate}>
-            {" "}
             Generate
           </button>
         </div>
-        <div className="call-button">
+        <div className="call-buttons">
           <button
             className="call-button"
             onClick={handleOneAndOneCall}
             disabled={!roomId}
           >
-            One-on-One
+            One-on-One Call
           </button>
           <button
             className="call-button"
@@ -63,6 +98,6 @@ const HomePage = () => {
       </div>
     </div>
   );
-};
+}
 
 export default HomePage;
---
+++ src/Config.js
@@ -0,0 +1,2 @@
+export const APP_ID = 745268637;
+export const SECRET = "dc3dd284691d5fa2eee8a11b394eec08";
